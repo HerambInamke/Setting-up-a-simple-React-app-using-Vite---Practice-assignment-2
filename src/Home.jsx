c7d29cf90892039e373dd5df369b1509f6315eb7
HerambInamke <heramb15012006@gmail.com>
2025-02-14 10:58:43 +0530

    Initial commit

---
+++ src/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Vansh</h1>
+      <p>Contact: vansh@example.com</p>
+    </div>
+  );
+};
+
+export default Home;
